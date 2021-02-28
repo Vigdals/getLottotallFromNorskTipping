eb0a7a7797221686a2f37bb6ed6fa32b43817862
Adrian <adrvig92@gmail.com>
2021-03-01 00:05:37 +0100

    added some codes

--- getLottotall.py
+++ getLottotall.py
@@ -1,5 +1,5 @@
 from helium import *
-from selenium.webdriver import FirefoxOptions
+from selenium.webdriver import FirefoxOptions #we need this, even tho we are just using helium
 import requests
 import re
 
@@ -9,7 +9,7 @@ start_firefox(options=options)
 
 go_to("https://www.norsk-tipping.no/lotteri/lotto/resultater")
 
-#selectorSearch = find_all(S(".winnernumbersContainer--3gVxG")) Denne traff på noko
+
 selectorSearch = find_all(S(".number--3Cvhn"))
 selectorFind = [item.web_element.text for item in selectorSearch]
 
@@ -17,9 +17,9 @@ print(selectorSearch)
 print("")
 print('[%s]' % ', '.join(map(str, selectorSearch)))
 
-f= open("lottoTallOutputRaw.txt","w+")
-f.write('[%s]' % ', '.join(map(str, selectorSearch)))
-f.close() 
+f= open("lottoTallOutputRaw.txt","w+") #open the file which we are writing the lottotall to
+f.write('[%s]' % ', '.join(map(str, selectorSearch))) #writes the lottotall to the file
+f.close()
 
 kill_browser()
 
@@ -45,7 +45,7 @@ for x in range(len(lottolinjer)): #loops through the list we made and prints it
 	outputFile.write("\n")
 	print(lottolinjer[x])
 
-def listToString(s):
+def listToString(s):#made this as a function
     
     # initialize an empty string 
     str1 = ""
@@ -54,15 +54,15 @@ def listToString(s):
     for ele in s:
         str1 += ele + " "
 
-    str1 = str1[:20] + "+ " + str1[20:]
+    str1 = str1[:20] + "+ " + str1[20:] #here i add a + icon between the first and last numbers due to the thing called tilleggstall
     # return string   
     return str1  
 
-print(listToString(lottolinjer))
+print(listToString(lottolinjer))#just a print for debugging
 
 f.close()
 outputFile.close()
 
-data = '{ "auth_token": "YOUR_AUTH_TOKEN", "text": "' + listToString(lottolinjer) + ' " }'
-print(data)
-response = requests.post('http://192.168.80.148:3030/widgets/welcome', data=data)
+data = '{ "auth_token": "YOUR_AUTH_TOKEN", "text": "' + listToString(lottolinjer) + ' " }'#set a variable to send to the dashboard
+print(data)#print it for debugging
+response = requests.post('http://192.168.80.148:3030/widgets/welcome', data=data)#sends the post to the server i got my dashboard on
